1066c6c92a26f5495650f4a467f874bd1962be30
MrDotYan <2564217790@qq.com>
2021-02-06 14:06:47 +0800

    feat:新增mongodb+maiadb+koa2的后台管理系统

---
+++ services/config/db.config.js
@@ -0,0 +1,21 @@
+exports.mongo = {
+  type: "MongoDB",
+  config: {
+    host: "127.0.0.1",
+    dbName: "vue3-element-plus-admin",
+    port: "27017",
+    user: "",
+    password: "",
+  }
+}
+
+exports.mariadb = {
+  type: "MariaDB",
+  config: {
+    host: "127.0.0.1",
+    user: '',
+    password: '',
+    database: '',
+    port: 3306
+  }
+}
